47f4f4a59360549a7df9aab9b3a08565c2bfd85c
flux <25628292+fluxionary@users.noreply.github.com>
2021-01-22 14:27:55 -0800

    change birch&palm to not start growing from inside the ground

--- birch/init.lua
+++ birch/init.lua
@@ -78,6 +78,7 @@ local function grow_new_birch_tree(pos)
 		minetest.get_node_timer(pos):start(math.random(240, 600))
 		return
 	end
+
 	minetest.place_schematic({x = pos.x - 2, y = pos.y, z = pos.z - 2}, birch.birchtree, "0", nil, true)
 end
 
